7295ccf0caa94836c45f862b9b6376b274fa00ad
Kirill Müller <krlmlr+r@mailbox.org>
2020-07-21 06:13:13 +0200

    Print stack trace to stderr

--- src/trace_back.c
+++ src/trace_back.c
@@ -2,6 +2,12 @@
 #include <R.h>
 #include <Rinternals.h>
 
+#include <execinfo.h>
+
+void* buf[10000];
+
 SEXP winch_trace_back() {
+  int size = backtrace(buf, sizeof(buf) / sizeof(*buf));
+  backtrace_symbols_fd(buf, size, 2);
   return R_NilValue;
 }
