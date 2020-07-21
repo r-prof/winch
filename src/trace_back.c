d2a808e63943dff963d3c2c93521dc1ec3cffac9
Kirill Müller <krlmlr+r@mailbox.org>
2020-07-21 06:01:22 +0200

    Start with libunwind

--- src/trace_back.c
+++ src/trace_back.c
@@ -2,24 +2,35 @@
 #include <R.h>
 #include <Rinternals.h>
 
-#include <execinfo.h>
+#define UNW_LOCAL_ONLY
+#include <libunwind.h>
 
 void* buf[10000];
 
 SEXP winch_trace_back() {
-  int size = backtrace(buf, sizeof(buf) / sizeof(*buf));
+  unw_context_t uc;
 
-  char** symbols = backtrace_symbols(buf, size);
-  if (!symbols) Rf_error("backtrace_symbols() failed to allocate.");
+  int unw;
+  unw = unw_getcontext(&uc);
+  if (unw != 0) Rf_error("unw_getcontext() error: %d", unw);
+
+  unw_cursor_t cursor;
+  unw = unw_init_local(&cursor, &uc);
+  if (unw != 0) Rf_error("unw_init_local() error: %d", unw);
+
+  char buf[1000];
+  unw_word_t off;
+  unw = unw_get_proc_name(&cursor, buf, sizeof(buf) / sizeof(*buf), &off);
+  buf[sizeof(buf) / sizeof(*buf) - 1] = '\0';
+  if (unw != 0 && unw != -UNW_ENOMEM) Rf_error("unw_get_proc_name() error: %d", unw);
+
+  int size = 1;
 
   // One less: ignore our call
   SEXP out = PROTECT(Rf_allocVector(STRSXP, size - 1));
 
-  for (int i = 0; i < size - 1; ++i) {
-    SET_STRING_ELT(out, i, Rf_mkCharCE(symbols[i + 1], CE_UTF8));
-  }
+  SET_STRING_ELT(out, 0, Rf_mkCharCE(buf, CE_UTF8));
 
   UNPROTECT(1);
-  free(symbols);
   return out;
 }
