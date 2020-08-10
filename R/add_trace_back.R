0e4f160dfafbc3a81f95d8a22eee569bfc30228d
Kirill Müller <krlmlr+r@mailbox.org>
2020-08-10 18:03:38 +0200

    .dylib

--- R/add_trace_back.R
+++ R/add_trace_back.R
@@ -9,7 +9,7 @@ winch_add_trace_back <- function(trace = rlang::trace_back(bottom = parent.frame
   # FIXME: This is artificial, remove when done
   #native_trace <- rep(native_trace, each = 3)
 
-  is_libr <- (basename(native_trace$pathname) == "libR.so")
+  is_libr <- grepl("libR[.](so|dylib|dll)", native_trace$pathname, ignore.case = TRUE)
   is_libr_idx <- which(is_libr)
 
   first_libr <- is_libr_idx[[length(is_libr_idx)]]
