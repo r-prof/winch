9e7b184cd8bbe711f0659e3602cc01133de2e532
Kirill Müller <krlmlr+r@mailbox.org>
2020-08-10 05:10:51 +0200

    Return IP address as string

--- src/trace_back.c
+++ src/trace_back.c
@@ -18,6 +18,10 @@ SEXP winch_trace_back() {
   unw = unw_init_local(&cursor, &uc);
   if (unw != 0) Rf_error("unw_init_local() error: %d", unw);
 
+  unw_proc_info_t pi;
+  unw = unw_get_proc_info(&cursor, &pi);
+  if (unw != 0) Rf_error("unw_get_proc_info() error: %d", unw);
+
   char buf[1000];
   unw_word_t off;
   unw = unw_get_proc_name(&cursor, buf, sizeof(buf) / sizeof(*buf), &off);
@@ -27,10 +31,24 @@ SEXP winch_trace_back() {
   int size = 1;
 
   // One less: ignore our call
-  SEXP out = PROTECT(Rf_allocVector(STRSXP, size - 1));
+  SEXP out_name = PROTECT(Rf_allocVector(STRSXP, size));
+  SET_STRING_ELT(out_name, 0, Rf_mkCharCE(buf, CE_UTF8));
 
-  SET_STRING_ELT(out, 0, Rf_mkCharCE(buf, CE_UTF8));
+  SEXP out_ip = PROTECT(Rf_allocVector(STRSXP, size));
+  char ip_buf[20];
+  sprintf(ip_buf, "%p", (void*)pi.start_ip);
+  //snprintf(ip_buf, sizeof(ip_buf) / sizeof(buf), "%p", (void*)pi.start_ip);
+  ip_buf[sizeof(ip_buf) / sizeof(*ip_buf) - 1] = '\0';
+  SET_STRING_ELT(out_ip, 0, Rf_mkCharCE(ip_buf, CE_UTF8));
 
-  UNPROTECT(1);
+  SEXP out = PROTECT(Rf_allocVector(VECSXP, 2));
+  SET_VECTOR_ELT(out, 0, out_name);
+  SET_VECTOR_ELT(out, 1, out_ip);
+
+  // FIXME: Read /proc/self/maps, map IP to library
+
+  // FIXME: Mimic ExtractSymbols() from gperftools -- use or rebuild addr2line
+
+  UNPROTECT(3);
   return out;
 }
