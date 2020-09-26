24dcf8964e5caab11373909b4bd86ded2edc396f
Kirill Müller <krlmlr+r@mailbox.org>
2020-09-26 06:09:46 +0200

    Remove code that uses R_GlobalContext

--- src/init.c
+++ src/init.c
@@ -11,7 +11,6 @@ extern SEXP winch_init_library();
 extern SEXP winch_trace_back();
 extern SEXP winch_trace_back_default_method();
 extern SEXP winch_call(SEXP function, SEXP env);
-extern SEXP winch_context();
 extern SEXP winch_stop(SEXP message);
 
 static const R_CallMethodDef CallEntries[] = {
@@ -19,7 +18,6 @@ static const R_CallMethodDef CallEntries[] = {
   {"winch_c_trace_back",                   (DL_FUNC) &winch_trace_back, 1},
   {"winch_c_trace_back_default_method",    (DL_FUNC) &winch_trace_back_default_method},
   {"winch_c_call",                         (DL_FUNC) &winch_call, 2},
-  {"winch_c_context",                      (DL_FUNC) &winch_context, 0},
   {"winch_c_stop",                         (DL_FUNC) &winch_stop, 1},
 
   {NULL, NULL, 0}
