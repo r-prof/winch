aca28e729f971dc432e39393b86fe0f404ae3298
Kirill Müller <krlmlr+r@mailbox.org>
2020-09-27 06:57:50 +0200

    More examples

---
+++ tests/example7.R
@@ -0,0 +1,17 @@
+library(winch)
+
+foo <- function() {
+  purrr::map(1, ~ bar())
+}
+
+bar <- function() {
+  winch_call(baz)
+}
+
+baz <- function() {
+  winch_add_trace_back()
+}
+
+if (winch_available() && requireNamespace("purrr")) {
+  foo()
+}
