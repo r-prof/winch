7240fc8ec32dd9ab022d2956aef00b3684702531
Kirill Müller <krlmlr+r@mailbox.org>
2020-09-27 07:50:56 +0200

    Skip if not available

--- tests/testthat/test-trace_back.R
+++ tests/testthat/test-trace_back.R
@@ -1,4 +1,6 @@
 test_that("data structure", {
+  skip_if_not(winch_available())
+
   out <- winch_trace_back()
   expect_s3_class(out, "data.frame")
   expect_identical(names(out), c("func", "ip", "pathname"))
