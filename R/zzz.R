1b7b00e8a8aa3a652751a410bef994eec68cb53c
Kirill Müller <krlmlr+r@mailbox.org>
2020-09-30 10:38:56 +0200

    Avoid <<-

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
+default_method <- NULL
+
 .onLoad <- function(libname, pkgname) {
-  set_default_method()
+  # Update the default method
+  ns <- asNamespace("winch")
+  ns[["default_method"]] <- .Call(winch_c_trace_back_default_method)
 }
