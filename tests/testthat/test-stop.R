b58613faa52bcb18a68e8d7780b26fd8b757e7e6
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-09 05:02:46 +0200

    Escape regex if necessary

--- tests/testthat/test-stop.R
+++ tests/testthat/test-stop.R
@@ -1,4 +1,4 @@
 test_that("winch_stop() works", {
   expect_error(winch_stop("foo"), "foo")
-  expect_error(winch_stop("\u2026"), enc2native("\u2026"))
+  expect_error(winch_stop("\u2026"), enc2native(glob2rx("\u2026")))
 })
