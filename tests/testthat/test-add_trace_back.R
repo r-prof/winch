26404e43dabfb038a86bcf4e1ad87f43cbce0287
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-24 04:28:36 +0200

    Make work with both rlang versions

--- tests/testthat/test-add_trace_back.R
+++ tests/testthat/test-add_trace_back.R
@@ -32,3 +32,27 @@ test_that("traceback changed if native code", {
     as.data.frame(foo_baz)
   })
 })
+
+
+test_that("traceback changed if native code (rlang < 1.0.0)", {
+  skip_if_not(winch_available())
+  skip_if(packageVersion("rlang") > "0.99")
+
+  foo <- function(fun) {
+    winch_call(fun)
+  }
+
+  bar <- function() {
+    rlang::trace_back()
+  }
+
+  baz <- function() {
+    winch_add_trace_back(rlang::trace_back())
+  }
+
+  foo_bar <- foo(bar)
+  foo_baz <- foo(baz)
+
+  expect_false(identical(foo_bar, foo_baz))
+  expect_true(any(grepl("/winch", foo_baz$calls)))
+})
