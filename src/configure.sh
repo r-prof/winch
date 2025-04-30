10d300df12ba9eefee3e4f42a05d48e56619113b
Kirill Müller <kirill@cynkra.com>
2025-04-30 14:38:09 +0200

    chore: Shellcheck

--- src/configure.sh
+++ src/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e
-cd `dirname $0`
+cd "$(dirname "$0")"
 
 # Anticonf (tm) script by Jeroen Ooms, Murat Tasan and Kirill Müller (2020)
 # This script will prefer cflags (specifically includefile dirs) and lib dirs
@@ -25,57 +25,58 @@ WINCH_LOCAL_LIBS=""
 
 # pkg-config values (if available)
 case "$OSTYPE" in
- darwin*)
-  # -lSystem sufficient on the Mac, no installation needed
-  # (will check anyway)
-  PKG_LIBUNWIND="-DHAVE_LIBUNWIND"
-  PKG_LIBS="-lSystem"
-  ;;
- msys*)
-  # handled in configure.win.sh
-  false
-  ;;
- solaris*)
-  PKG_LIBBACKTRACE="-DHAVE_LIBBACKTRACE"
-  ;;
- *)
-  PKG_LIBUNWIND="-DHAVE_LIBUNWIND"
-  PKG_LIBBACKTRACE="-DHAVE_LIBBACKTRACE"
+  darwin*)
+    # -lSystem sufficient on the Mac, no installation needed
+    # (will check anyway)
+    PKG_LIBUNWIND="-DHAVE_LIBUNWIND"
+    PKG_LIBS="-lSystem"
+    ;;
+  msys*)
+    # handled in configure.win.sh
+    false
+    ;;
+  solaris*)
+    PKG_LIBBACKTRACE="-DHAVE_LIBBACKTRACE"
+    ;;
+  *)
+    PKG_LIBUNWIND="-DHAVE_LIBUNWIND"
+    PKG_LIBBACKTRACE="-DHAVE_LIBBACKTRACE"
 
-  if [ `command -v pkg-config` ]; then
-    PKGCONFIG_CFLAGS=`pkg-config --cflags --silence-errors ${PKG_CONFIG_NAME} || true`
-    PKGCONFIG_LIBS=`pkg-config --libs --silence-errors ${PKG_CONFIG_NAME} || true`
-    PKGCONFIG_MODVERSION=`pkg-config --modversion --silence-errors ${PKG_CONFIG_NAME} || true`
-  fi
+    if [ "$(command -v pkg-config)" ]; then
+      PKGCONFIG_CFLAGS=$(pkg-config --cflags --silence-errors ${PKG_CONFIG_NAME} || true)
+      PKGCONFIG_LIBS=$(pkg-config --libs --silence-errors ${PKG_CONFIG_NAME} || true)
+      PKGCONFIG_MODVERSION=$(pkg-config --modversion --silence-errors ${PKG_CONFIG_NAME} || true)
+    fi
 
-  # Note that cflags may be empty in case of success
-  if [ "$INCLUDE_DIR" ] || [ "$LIB_DIR" ]; then
-    echo "Found INCLUDE_DIR and/or LIB_DIR!"
-    PKG_CFLAGS="-I$INCLUDE_DIR $PKG_CFLAGS"
-    PKG_LIBS="-L$LIB_DIR $PKG_LIBS"
-  elif [ "$PKGCONFIG_CFLAGS" ] || [ "$PKGCONFIG_LIBS" ]; then
-    echo "Found pkg-config cflags and libs ($PKG_CONFIG_NAME $PKGCONFIG_MODVERSION)!"
-    PKG_CFLAGS="$PKGCONFIG_CFLAGS"
-    PKG_LIBS="$PKGCONFIG_LIBS"
-  else
-    echo "No cflags and libs found!"
-    PKG_CFLAGS=""
-  fi
-  ;;
+    # Note that cflags may be empty in case of success
+    if [ "$INCLUDE_DIR" ] || [ "$LIB_DIR" ]; then
+      echo "Found INCLUDE_DIR and/or LIB_DIR!"
+      PKG_CFLAGS="-I$INCLUDE_DIR $PKG_CFLAGS"
+      PKG_LIBS="-L$LIB_DIR $PKG_LIBS"
+    elif [ "$PKGCONFIG_CFLAGS" ] || [ "$PKGCONFIG_LIBS" ]; then
+      echo "Found pkg-config cflags and libs ($PKG_CONFIG_NAME $PKGCONFIG_MODVERSION)!"
+      PKG_CFLAGS="$PKGCONFIG_CFLAGS"
+      PKG_LIBS="$PKGCONFIG_LIBS"
+    else
+      echo "No cflags and libs found!"
+      PKG_CFLAGS=""
+    fi
+    ;;
 esac
 
 if [ -n "$PKG_LIBUNWIND" ]; then
 
 # Find compiler
-CC=`${R_HOME}/bin/R CMD config CC`
-CFLAGS=`${R_HOME}/bin/R CMD config CFLAGS`
-CPPFLAGS=`${R_HOME}/bin/R CMD config CPPFLAGS`
+CC=$("${R_HOME}/bin/R" CMD config CC)
+CFLAGS=$("${R_HOME}/bin/R" CMD config CFLAGS)
+CPPFLAGS=$("${R_HOME}/bin/R" CMD config CPPFLAGS)
 
 # Test configuration
+# shellcheck disable=SC2086
 echo "#include $PKG_TEST_HEADER" | ${CC} ${CPPFLAGS} ${PKG_CFLAGS} ${CFLAGS} -E -xc - >/dev/null 2>&1 || R_CONFIG_ERROR=1;
 
 # Customize the error
-if [ $R_CONFIG_ERROR ]; then
+if [ "$R_CONFIG_ERROR" ]; then
   echo "------------------------- ANTICONF ERROR ---------------------------"
   echo "$PKG_CONFIG_NAME was not found. Try installing:"
   echo " * deb: $PKG_DEB_NAME (Debian, Ubuntu, etc)"
@@ -84,7 +85,7 @@ if [ $R_CONFIG_ERROR ]; then
   echo "If $PKG_CONFIG_NAME is already installed, check that either:"
   echo "'pkg-config' is in your PATH AND PKG_CONFIG_PATH contains"
   echo "a $PKG_CONFIG_NAME.pc file."
-  echo "If it cannot detect $PGK_CONFIG_NAME, you can set INCLUDE_DIR"
+  echo "If it cannot detect $PKG_CONFIG_NAME, you can set INCLUDE_DIR"
   echo "and LIB_DIR manually via:"
   echo "R CMD INSTALL --configure-vars='INCLUDE_DIR=... LIB_DIR=...'"
   echo "--------------------------------------------------------------------"
@@ -98,7 +99,7 @@ if [ -n "$PKG_LIBBACKTRACE" ]; then
 
   # Only if libbacktrace can be configured and built
   # (need to use make because environment variables are set in etc/Makeconf)
-  if make -f ${R_HOME}/etc${R_ARCH}/Makeconf -f Makevars.configure; then
+  if make -f "${R_HOME}"/etc"${R_ARCH}"/Makeconf -f Makevars.configure; then
     WINCH_LOCAL_LIBS="local/lib/libbacktrace.a"
   else
     PKG_LIBBACKTRACE=""
